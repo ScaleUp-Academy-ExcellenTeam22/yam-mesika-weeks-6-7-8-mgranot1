817ac58a6e7b0b5c09f4fe1dcbba127780c4bdc7
mgranot1 <80787986+mgranot1@users.noreply.github.com>
2022-05-03 17:29:59 +0300

    try_2

--- 7_3.py
+++ 7_3.py
@@ -4,14 +4,15 @@ class Message:
     A Message Representation Class
 
     """
-    def __init__(self, title, body, sender, receiver):
+    def __init__(self, title: str , body: str, sender: str, receiver: str):
         self.title = title
         self.body = body
         self.sender = sender
         self.receiver = receiver
 
     def __str__(self):
-        return 'Message (title=' + self.title + ' ,body=' + self.body + ' ,sender=' + self.sender+ ' ,receiver=' + self.receiver+ ')'
+         return 'Message (title= {} ,body= {} ,sender= {} ,receiver= {} )'.format(self.title, self.body, self.sender,self.receiver)
+
 
     def __len__(self):
         return len(self.body)
